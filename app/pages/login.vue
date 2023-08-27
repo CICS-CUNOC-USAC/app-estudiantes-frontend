55fc4431e5e06708cf8c0d6d363d2f5e899870d6
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-08-27 17:53:16 -0600

    Installed google fonts module, moved components to app/components dir

---
+++ app/pages/login.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <v-form>
+      <v-text-field
+        v-model="email"
+        label="Email"
+        type="email"
+        :rules="emailRules"
+        required
+      />
+      <v-text-field
+        v-model="password"
+        label="Password"
+        type="password"
+        :rules="passwordRules"
+        required
+      />
+      <v-btn
+        @click="login"
+      >
+        Iniciar sesión
+      </v-btn>
+    </v-form>
+  </section>
+</template>
+<script>
+export default {
+  setup () {
+    definePageMeta({
+      layout: 'login'
+    })
+  },
+  data () {
+    return {
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 8 || 'Password must be at least 8 characters'
+      ]
+    }
+  },
+  methods: {
+    login
+    () {
+      console.log('login')
+      // this.$router.push('/dashboard')
+    }
+  }
+}
+</script>
+<style lang="">
+
+</style>
--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -1,10 +1,16 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { md3 } from 'vuetify/blueprints'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    blueprint: md3,
     defaults: {
+      VBtn: {
+        variant: 'tonal',
+        color: 'orange-accent-4'
+      },
       global: {
         ripple: false
       }
