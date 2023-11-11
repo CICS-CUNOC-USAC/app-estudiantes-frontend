55cc2d76b3198fdafde37283baa2e0da21d19455
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-11-11 00:01:39 -0600

    feat: Add media store and createManual function to admin-manuals store

--- app/stores/admin-manuals.ts
+++ app/stores/admin-manuals.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { type FetchError } from 'ofetch'
+import type { MediaResponse } from './media'
 export type Manual = {
   id: number
   name: string
@@ -6,6 +9,7 @@ export type Manual = {
   url: null
   created_at: Date
   updated_at: Date
+  media: MediaResponse
 }
 
 export type ManualsAdminResponse = {
@@ -23,11 +27,16 @@ type Metadata = {
   total_pages: number
 }
 
+export type ManualPayload = {
+  name: string
+  description: string
+  file?: File[] | undefined
+}
+
 export const useAdminManualsStore = defineStore('admin-manuals', () => {
   const loading = ref<boolean>(false)
   const manuals = ref<Manual[] | undefined>(undefined)
   const manual = ref<Manual | null>(null)
-  const userAuth = useCookie('cicsapp-user-token')
   const responseMeta = ref<Metadata | null>(null)
 
   const fetchAllManuals = async (params?: {
@@ -38,7 +47,7 @@ export const useAdminManualsStore = defineStore('admin-manuals', () => {
   }) => {
     loading.value = true
 
-    const { data, error } = await useCustomFetch<ManualsAdminResponse>(
+    const { data } = await useCustomFetch<ManualsAdminResponse>(
       '/manuals/admin',
       { params }
     )
@@ -50,28 +59,95 @@ export const useAdminManualsStore = defineStore('admin-manuals', () => {
     }
     loading.value = false
   }
-
-  // We'll fetch individual items in each page using nuxt's fetch hook and not functions inside stores
-  const fetchManualById = async (id: number | string) => {
+  const fetchManualById = (id: number | string) => {
     loading.value = true
-    const { data, error } = await useLazyFetch<Manual>(
-      `${import.meta.env.VITE_API_BASE_URL}/manuals/admin/${+id}`,
-      {
-        headers: userAuth.value
-          ? {
-              Authorization: `Bearer ${userAuth.value}`,
-              'Content-Type': 'application/json'
-            }
-          : { 'Content-Type': 'application/json' }
-      }
-    )
-    manual.value = data.value
+
+    const data = $api<Manual>(`manuals/admin/${+id}`)
+
     loading.value = false
+    return data
   }
 
-  const updateManual = async (id: number | string, payload: Manual) => {}
+  const createManual = async (payload: ManualPayload) => {
+    loading.value = true
+    try {
+      // First, upload the pdf file using media store to get the media item id
+      const uploadedMedia = await useMediaStore().postMedia({
+        file: payload.file?.[0],
+        path: 'manuals',
+        attach_type: 'manual'
+      })
+      // Then, create the manual using the media item id
+      const response = await $api<Manual>('/manuals/admin', {
+        method: 'POST',
+        body: {
+          ...payload,
+          media_id: uploadedMedia?.id
+        }
+      })
+      useSnackbarStore().showSnackbar({
+        title: 'Recurso creado',
+        message: 'Manual creado correctamente',
+        type: SnackbarType.SUCCESS
+      })
+      return response
+    } catch (error) {
+      useSnackbarStore().showSnackbar({
+        title: 'Error al crear el recurso',
+        message: (error as any).data.error,
+        type: SnackbarType.ERROR
+      })
+    } finally {
+      loading.value = false
+    }
+  }
 
-  const deleteManual = async (id: number | string) => {}
+  const updateManual = async (id: number | string, payload: ManualPayload) => {
+    loading.value = true
+    try {
+      const response = await $api<Manual>(`/manuals/admin/${+id}`, {
+        method: 'PATCH',
+        body: payload
+      })
+      useSnackbarStore().showSnackbar({
+        title: 'Recurso actualizado',
+        message: 'Manual actualizado correctamente',
+        type: SnackbarType.SUCCESS
+      })
+      return response
+    } catch (error) {
+      useSnackbarStore().showSnackbar({
+        title: 'Error al actualizar el recurso',
+        message: (error as any).data.error,
+        type: SnackbarType.ERROR
+      })
+    } finally {
+      loading.value = false
+    }
+  }
+
+  const deleteManual = async (id: number | string) => {
+    loading.value = true
+    try {
+      const response = await $api<Manual>(`/manuals/admin/${+id}`, {
+        method: 'DELETE'
+      })
+      useSnackbarStore().showSnackbar({
+        title: 'Recurso eliminado',
+        message: 'Manual eliminado correctamente',
+        type: SnackbarType.SUCCESS
+      })
+      return response
+    } catch (error) {
+      useSnackbarStore().showSnackbar({
+        title: 'Error al eliminar el recurso',
+        message: (error as any).data.error,
+        type: SnackbarType.ERROR
+      })
+    } finally {
+      loading.value = false
+    }
+  }
 
   return {
     loading,
@@ -80,6 +156,7 @@ export const useAdminManualsStore = defineStore('admin-manuals', () => {
     responseMeta,
     fetchAllManuals,
     fetchManualById,
+    createManual,
     updateManual,
     deleteManual
   }
