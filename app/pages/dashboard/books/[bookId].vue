cacfd31a03637c0cb612a096e8de7865a78bbc07
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2023-11-16 14:26:04 -0600

    refactor: Refactored UI components and add book author information

--- app/pages/dashboard/books/[bookId].vue
+++ app/pages/dashboard/books/[bookId].vue
@@ -1,13 +1,43 @@
 <template>
   <main>
     <div v-if="book">
-      <v-btn prepend-icon="mdi-arrow-left" class="my-3" @click="$router.back()">
-        Regresar
-      </v-btn>
+      <v-row class="mb-1" no-gutters>
+        <v-col cols="12" md="2" class="mr-2 my-2">
+          <v-btn block prepend-icon="mdi-arrow-left" @click="$router.back()">
+            Regresar
+          </v-btn>
+        </v-col>
+        <v-col cols="12" md="2" class="mr-2 my-2">
+          <v-btn
+            block
+            color="gray"
+            :href="book.media.url"
+            target="_blank"
+            prepend-icon="mdi-download"
+          >
+            Descargar
+          </v-btn>
+        </v-col>
+      </v-row>
+
       <h1 class="mb-4">{{ book?.name }}</h1>
       <p class="mb-4">
         {{ book?.description }}
       </p>
+      <h2 class="mb-4">Autor</h2>
+      <p class="mb-4">
+        {{ book?.author }}
+      </p>
+      <!-- <a
+        :href="`https://www.google.com/search?q=${book?.author}`"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <v-btn color="primary" class="mb-4">
+          Buscar más libros de {{ book?.author }}
+        </v-btn>
+      </a> -->
+
       <ClientOnly class="d-flex justify-center w-100">
         <PdfPreview :pdf-url="book.media.url" :show-zoom="false" fit-to-page />
         <template #fallback>
