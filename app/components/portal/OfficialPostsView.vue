e2ed157441742d697bf9f24e9f8daf79d5410164
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2024-05-22 10:10:11 -0600

    feat: Add publicaciones page and OfficialPosts related components

---
+++ app/components/portal/OfficialPostsView.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="(item, index) in data"
+      :key="index"
+      cols="12"
+      xs="12"
+      sm="12"
+      lg="4"
+    >
+      <InfoCard
+        full-height
+        :title="item.title"
+        :description="item.description"
+        :route="`/portal/post/${item.link}`"
+      />
+    </v-col>
+  </v-row>
+</template>
+<script setup lang="ts">
+import InfoCard from '../cards/InfoCard.vue'
+
+const { data } = useFetch<
+  {
+    title: string
+    description: string
+    link: string
+    original_link: string
+    posted_since: string
+  }[]
+>('/api/official-posts')
+</script>
+<style lang="scss" scoped></style>
