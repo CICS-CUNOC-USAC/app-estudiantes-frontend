d2e0fed53ffbcb128d42c91842d903955e0880d7
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2024-05-22 10:09:54 -0600

    feat: Create official post detail API to fetch and process content from external website

---
+++ app/server/api/official-posts.ts
@@ -0,0 +1,47 @@
+import { JSDOM } from 'jsdom'
+
+export default defineEventHandler(async (event) => {
+  const BASE_URL = 'http://ingenieria.cunoc.usac.edu.gt/portal/index.php'
+  const query = getQuery(event)
+  const page = query.page || 1
+  const html = (await $fetch(BASE_URL, {
+    params: {
+      page
+    }
+  })) as string
+  const doc = new JSDOM(html).window.document
+  const mainPostsContainer = doc.querySelector('#contenido')
+  if (!mainPostsContainer) return {}
+
+  const postsContainers = mainPostsContainer.querySelectorAll('.featurette')
+  if (!postsContainers.length) return []
+  const posts = Array.from(postsContainers).map((postContainer) => {
+    const postTitle = postContainer.querySelector('h2')?.textContent
+    const postDescription = postContainer
+      .querySelector('p')
+      ?.firstChild?.textContent?.trim()
+    // .replace(/\n/g, '')
+    const postLink = postContainer
+      .querySelector('p>a')
+      ?.getAttribute('href')
+      ?.split('/')
+
+    const postLinkFinal = postLink?.[5] + '/' + postLink?.[6]
+
+    const postPostedSince = postContainer
+      .querySelector('blockquote>footer')
+      ?.textContent?.trim()
+      .replace(/\n/g, '')
+      .replace(/\t/g, '')
+      .replace(/el/g, 'el ')
+
+    return {
+      title: postTitle,
+      description: postDescription,
+      link: postLinkFinal,
+      original_link: postLink?.join('/'),
+      posted_since: postPostedSince
+    }
+  })
+  return posts
+})
