45c809c7949ef5a24b1bae9b81c64d5a1008c914
Benjamin <carlosbpac@gmail.com>
2024-07-21 04:20:20 -0600

    refactor: Simplify table header rendering in horarios.vue

--- app/components/portal/horarios/CursoHorario.vue
+++ app/components/portal/horarios/CursoHorario.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container style="padding: 4px">
-    <v-card class="mx-auto" max-width="350" min-width="180">
+    <v-card
+      class="mx-auto"
+      :class="getCorrectClassCard(curso)"
+      max-width="350"
+      min-width="180"
+    >
       <template #actions>
         <v-container>
           <v-row justify="center">
@@ -40,6 +45,23 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    getCorrectClassCard(curso) {
+      if (curso.semestre == 'III') {
+        return 'card_color_green'
+      } else if (curso.semestre == 'IV') {
+        return 'card_color_yellow'
+      } else if (curso.semestre == 'V') {
+        return 'card_color_smoth_green'
+      } else if (curso.semestre == 'VI') {
+        return 'card_color_orage'
+      } else if (curso.semestre == 'VIII') {
+        return 'card_color_magenta'
+      } else if (curso.semestre == 'X') {
+        return 'card_color_blue'
+      }
+    }
   }
 }
 </script>
@@ -51,5 +73,35 @@ export default {
   margin: 0;
   padding: 0;
   /* Elimina márgenes adicionales */
+}
+
+.card_color_green {
+  background-color: #4caf50;
+  color: white;
+}
+
+.card_color_yellow {
+  background-color: #ffeb3b;
+  color: black;
+}
+
+.card_color_smoth_green {
+  background-color: #8bc34a;
+  color: white;
+}
+
+.card_color_orage {
+  background-color: #ff9800;
+  color: white;
+}
+
+.card_color_magenta {
+  background-color: #c74b74;
+  color: white;
+}
+
+.card_color_blue {
+  background-color: #2196f3;
+  color: white;
 }
 </style>
