972557491adf10e5e2538a53bce5c4f623987124
Benjamin <carlosbpac@gmail.com>
2024-07-21 03:03:38 -0600

    feat: Add CursoHorario component for displaying course schedule information

---
+++ app/components/portal/horarios/CursoHorario.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container style="padding: 4px">
+    <v-card class="mx-auto" max-width="350" min-width="180">
+      <template #actions>
+        <v-container>
+          <v-row justify="center">
+            <v-col class="text-center">
+              <v-row>
+                <v-col class="text-center small-text">
+                  {{ curso.curso }}
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="text-center small-text">
+                  {{ curso.codigo }}
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="text-center small-text">
+                  {{ curso.docente }}
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="text-center small-text">
+                  {{ curso.semestre }}
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    curso: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.small-text {
+  font-size: 12px;
+  /* Ajusta el tamaño según sea necesario */
+  margin: 0;
+  padding: 0;
+  /* Elimina márgenes adicionales */
+}
+</style>
--- app/pages/portal/general/horarios.vue
+++ app/pages/portal/general/horarios.vue
@@ -1,74 +1,63 @@
 <template>
   <v-container>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      item-key="hora"
-      items-per-page="100"
-    >
+    <v-table class="elevation-1">
       <template #top>
         <v-toolbar flat>
-          <v-toolbar-title>Horarios</v-toolbar-title>
+          <v-toolbar-title>Horarios de Clases</v-toolbar-title>
         </v-toolbar>
       </template>
-      <template #item.s1="{ item }">
-        <v-container>
-          <v-card :variant="flat" class="mx-auto" max-width="350">
-            <template #actions>
-              <v-container>
-                <v-row justify="center">
-                  <v-col class="text-center">
-                    <v-row>
-                      <v-col class="text-center small-text">
-                        {{ item.s1.curso }}
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="text-center small-text">
-                        {{ item.s1.codigo }}
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="text-center small-text">
-                        {{ item.s1.docente }}
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col class="text-center small-text">
-                        {{ item.s1.semestre }}
-                      </v-col>
-                    </v-row>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </template>
-          </v-card>
-        </v-container>
-      </template>
-    </v-data-table>
+      <thead>
+        <tr>
+          <th rowspan="2" style="width: 150px">Hora</th>
+          <th colspan="12" class="text-center">Salón</th>
+        </tr>
+        <tr>
+          <th
+            v-for="header in headers[1].children"
+            :key="header.value"
+            class="text-center"
+          >
+            {{ header.title }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.hora">
+          <td class="text-center">{{ item.hora }}</td>
+          <td v-for="header in headers[1].children" :key="header.value">
+            <CursoHorario
+              v-if="item[header.value]"
+              :curso="item[header.value]"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
   </v-container>
 </template>
 
 <script lang="ts">
+import CursoHorario from '~/components/portal/horarios/CursoHorario.vue'
+
 export default {
   name: 'Horarios',
+  components: { CursoHorario },
   data() {
     return {
       headers: [
         { title: 'Hora', value: 'hora' },
         {
           title: 'Salón',
-          align: 'center',
           children: [
             { title: '1', value: 's1' },
             { title: '3', value: 's2' },
             { title: '5', value: 's3' },
-            { title: '6', value: 's4' },
+            // { title: '6', value: 's4' },
             { title: '7', value: 's5' },
-            { title: '8', value: 's6' },
+            // { title: '8', value: 's6' },
             { title: '10', value: 's7' },
             { title: 'Salón Proyecciones', value: 's8' },
-            { title: 'Salón TICS', value: 's9' },
+            // { title: 'Salón TICS', value: 's9' },
             { title: 'Hugo Pineda', value: 's10' },
             { title: 'Salón Dibujo', value: 's11' },
             { title: 'Laboratorio de Aguas', value: 's12' }
@@ -77,69 +66,57 @@ export default {
       ],
       items: [
         {
-          hora: '13:00 - 14:40',
+          hora: '13:40 - 14:30',
           s1: {
             curso: 'Manejo e Implementación de Archivos',
             docente: '',
             codigo: '2812',
-            semestre: 'III'
-          },
-          s5: {
-            curso: 'Introducción a la Programación 1',
+            semestre: 'VI'
+          }
+        },
+        {
+          hora: '14:30 - 15:20',
+          s1: {
+            curso: 'Organización de Lenguajes y Compiladores 2',
             docente: '',
-            codigo: '2796',
-            semestre: ''
+            codigo: '2810',
+            semestre: 'VI'
           },
           s10: {
             curso: 'Introducción a la Programación 1',
             docente: '',
             codigo: '2796',
-            semestre: ''
+            semestre: 'III'
           }
         },
         {
-          hora: '14:40 - 15:20',
-          s1: {
-            curso: 'Organización de Lenguajes y Compiladores 2',
-            docente: '',
-            codigo: '2810',
-            semestre: ''
-          },
-          s8: {
-            curso: 'Práctica Final',
-            docente: '',
-            codigo: '2829',
-            semestre: ''
-          }
-        },
-        {
-          hora: '15:20 - 16:00',
+          hora: '15:20 - 16:10',
           s1: {
             curso: 'Arquitectura de Computadoras y Ensambladores 1',
             docente: '',
             codigo: '2811',
-            semestre: ''
-          },
-          s7: {
-            curso: 'Matemática de Computo 2',
-            docente: '',
-            codigo: '2799',
-            semestre: ''
-          },
-          s12: {
-            curso: 'Matemática de Computo 2',
-            docente: '',
-            codigo: '2799',
-            semestre: ''
+            semestre: 'VI'
           }
         },
         {
-          hora: '16:00 - 17:00',
+          hora: '16:10 - 17:00',
           s1: {
             curso: 'Teoría de Sistemas 1',
             docente: '',
             codigo: '2808',
-            semestre: ''
+            semestre: 'VI'
+          },
+          s10: {
+            curso: 'Práctica Final',
+            docente: '',
+            codigo: '2829',
+            semestre: 'X'
+          },
+          s11: {
+            curso: 'Lenguajes Formales y de Programación',
+            docente: '',
+            codigo: '2798',
+            semestre: 'IV'
           }
         },
         {
@@ -150,11 +127,17 @@ export default {
             codigo: '2822',
             semestre: ''
           },
-          s3: {
+          s2: {
             curso: 'Sistemas de Bases de Datos 2',
             docente: '',
             codigo: '2821',
             semestre: ''
+          },
+          s12: {
+            curso: 'Matemática de Computo 2',
+            docente: '',
+            codigo: '2799',
+            semestre: 'IV'
           }
         },
         {
@@ -163,13 +146,25 @@ export default {
             curso: 'Introducción a la Programación y Computación 2',
             docente: '',
             codigo: '2800',
-            semestre: ''
+            semestre: 'IV'
           },
-          s3: {
-            curso: 'Sistemas de Bases de Datos 2',
+          s4: {
+            curso: 'Redes de Computadoras 2',
             docente: '',
-            codigo: '2821',
-            semestre: ''
+            codigo: '2820',
+            semestre: 'VIII'
+          },
+          s8: {
+            curso: 'Economía',
+            docente: '',
+            codigo: '2809',
+            semestre: 'VI'
+          },
+          s10: {
+            curso: 'Seminario de Investigación',
+            docente: '',
+            codigo: '2837',
+            semestre: 'X'
           }
         },
         {
@@ -213,16 +208,15 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    filtrarSalones() {}
   }
 }
 </script>
 
 <style scoped lang="scss">
-.small-text {
-  font-size: 12px;
-  /* Ajusta el tamaño según sea necesario */
-  margin: 0;
-  padding: 0;
-  /* Elimina márgenes adicionales */
+.text-center {
+  text-align: center;
 }
 </style>
