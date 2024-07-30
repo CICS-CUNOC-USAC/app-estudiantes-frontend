a9f306dfa937b4dd4667d77a5adb6c47f587219c
Benjamin <carlosbpac@gmail.com>
2024-07-30 17:40:47 -0600

    feat: Add types for ScheduleCourse, ScheduleLine, Period, and Course

--- app/utils/types/schedule-courses.ts
+++ app/utils/types/schedule-courses.ts
@@ -1,13 +1,41 @@
 export type ScheduleCourse = {
-  curso: string;
-  docente: string;
-  codigo: string;
-  semestre: string;
-  codigo_salon: string;
-  nombre_salon: string;
+  curso: string
+  docente: string
+  codigo: string
+  semestre: string
+  codigo_salon: string
+  nombre_salon: string
 }
 
 export type ScheduleLine = {
-  hora: string;
-  cursos: Array<ScheduleCourse>;
+  hora: string
+  cursos: Array<ScheduleCourse>
+}
+
+export type Period = {
+  weekday_id: number
+  start_time: string
+  end_time: string
+  weekay: {
+    name: string
+  }
+}
+
+export type Course = {
+  id: number
+  career_code: number
+  course_code: string
+  section_id: number
+  classroom_id: number
+  periods: Array<Period>
+  career_course: {
+    semester: number
+    field: number
+  }
+  section: {
+    name: string
+  }
+  classroom: {
+    name: string
+  }
 }
