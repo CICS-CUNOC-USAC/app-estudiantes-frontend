89d98aea6948461f2644955cb1600b7d98c48f6b
FernandoJRR <fercitotron@gmail.com>
2024-08-09 17:43:00 -0600

    feat: componentize ClassScheduleV1

--- app/pages/portal/general/horarios.vue
+++ app/pages/portal/general/horarios.vue
@@ -1,17 +1,32 @@
 <template>
-  <v-container v-if="!loading">
-    <ClassScheduleV1 :courses="scheduleCourses" />
-  </v-container>
+  <main>
+    <h1 class="mb-4">Horario de Clases V1</h1>
+    <v-container
+      v-if="!loadingHours && !loadingClassrooms && !loadingSchedules"
+    >
+      <ClassScheduleV1
+        :hours="hours!"
+        :classrooms="classrooms!"
+        :schedules="schedules!"
+      />
+    </v-container>
+  </main>
 </template>
 
 <script setup lang="ts">
 import ClassScheduleV3 from '~/components/portal/horarios/ClassScheduleV3.vue'
 import ClassScheduleV2 from '~/components/portal/horarios/ClassScheduleV2.vue'
 import ClassScheduleV1 from '~/components/portal/horarios/ClassScheduleV1.vue'
-import type { ScheduleResponse } from '~/utils/types/schedule-courses'
+import type { Classroom, Course, Hour } from '~/utils/types/schedule-courses'
 
-const { data: scheduleCourses, pending: loading } =
-  useCustomLazyFetch<ScheduleResponse>(`/schedules/courses`)
+const { data: hours, pending: loadingHours } =
+  useCustomLazyFetch<Array<Hour>>(`hours`)
+
+const { data: classrooms, pending: loadingClassrooms } =
+  useCustomLazyFetch<Array<Classroom>>(`classrooms`)
+
+const { data: schedules, pending: loadingSchedules } =
+  useCustomLazyFetch<Array<Course>>(`schedules/courses`)
 
 /*
 export default {
