c8ea2b21b612e80612776901ced515958d44e1bc
FernandoJRR <fercitotron@gmail.com>
2024-08-09 18:07:23 -0600

    feat: add schedule component to schedule administration index.vue

--- app/pages/admin/schedules/index.vue
+++ app/pages/admin/schedules/index.vue
@@ -26,13 +26,36 @@
       </v-col>
       -->
     </v-row>
+    <v-row>
+      <v-container
+        v-if="!loadingHours && !loadingClassrooms && !loadingSchedules"
+      >
+        <ClassScheduleV1
+          :hours="hours!"
+          :classrooms="classrooms!"
+          :schedules="schedules!"
+        />
+      </v-container>
+    </v-row>
   </main>
 </template>
 <script setup lang="ts">
+import ClassScheduleV1 from '~/components/portal/horarios/ClassScheduleV1.vue'
+import type { Classroom, Course, Hour } from '~/utils/types/schedule-courses'
+
 definePageMeta({
   layout: 'admin'
 })
 
 const name = ref('')
+
+const { data: hours, pending: loadingHours } =
+  useCustomLazyFetch<Array<Hour>>(`hours`)
+
+const { data: classrooms, pending: loadingClassrooms } =
+  useCustomLazyFetch<Array<Classroom>>(`classrooms`)
+
+const { data: schedules, pending: loadingSchedules } =
+  useCustomLazyFetch<Array<Course>>(`schedules/courses`)
 </script>
 <style lang="scss" scoped></style>
