7b369df95cac3d5c6d5b71d532a2808c58f9be1c
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2024-11-17 03:31:32 -0600

    feat: Refactor theme management to use composition API and improve theme initialization logic

--- app/pages/portal/extras/config.vue
+++ app/pages/portal/extras/config.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    {{ theme }}
     <header class="mb-4">
       <h1>Configuración</h1>
       <CButton icon="lucide:arrow-left" to="/" label="Regresar" />
@@ -14,28 +15,37 @@
         Elije un tema general para la apariencia de la aplicación. Puedes
         seleccionar entre colores y claros y oscuros.
       </p>
-      <PButton label="Test" unstyled/>
+      <PButton label="Test" unstyled />
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        <PSelect v-model="selectedTheme" :options checkmark :highlightOnSelect="false" @value-change="handleThemeChange" />
+        <PSelect
+          v-model="theme"
+          :options
+          checkmark
+          :highlightOnSelect="false"
+          @value-change="handleThemeChange"
+        />
       </div>
     </section>
   </main>
 </template>
 <script setup lang="ts">
-import CButton from '~/components/primitives/button/CButton.vue';
-import { allThemesPrimaries } from '~/themes/pThemes';
-import { updatePrimaryPalette } from '@primevue/themes';
+import CButton from '~/components/primitives/button/CButton.vue'
+import { allThemesPrimaries } from '~/themes/pThemes'
+import { updatePrimaryPalette } from '@primevue/themes'
 
-const pv = usePrimeVue();
-const selectedTheme = ref(pv.config.theme.preset.name)
+const configsStore = useConfigsStore()
+const { theme } = storeToRefs(configsStore)
+const { changeTheme: handleThemeChange } = configsStore
+
+// const pv = usePrimeVue();
+// const selectedTheme = ref(pv.config.theme.preset.name)
 
 const options = allThemesPrimaries.map((theme) => theme.name)
 
-const handleThemeChange = (newTheme: string) => {
-  const selectedTheme = allThemesPrimaries.find((theme) => theme.name === newTheme)
-  selectedTheme && updatePrimaryPalette(selectedTheme.values)
-  pv.config.theme.preset.name = newTheme
-}
-
+// const handleThemeChange = (newTheme: string) => {
+//   const selectedTheme = allThemesPrimaries.find((theme) => theme.name === newTheme)
+//   selectedTheme && updatePrimaryPalette(selectedTheme.values)
+//   pv.config.theme.preset.name = newTheme
+// }
 </script>
 <style lang="postcss" scoped></style>
