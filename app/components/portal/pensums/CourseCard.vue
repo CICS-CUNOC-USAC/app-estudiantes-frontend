acb9912b3cd75c12d6cb7ebbfc444c59001d8d1f
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2024-12-01 22:37:29 -0600

    refactor: Update CourseCard tailwind classes

--- app/components/portal/pensums/CourseCard.vue
+++ app/components/portal/pensums/CourseCard.vue
@@ -1,24 +1,24 @@
 <template>
-  <component
-    class="flex min-h-20 -translate-x-0.5 -translate-y-0.5 flex-row overflow-hidden rounded-[10px] border border-surface-950/75 bg-surface-50 shadow-[2px_2px_0_0_rgba(0,0,0,1)] duration-300 ease-in-out text-color dark:bg-gray-700"
+  <div
+    class="flex min-h-20 rounded-[10px] border border-surface-950/75 bg-surface-50 shadow-[2px_2px_0_0_rgba(0,0,0,1)] ease-in-out text-color dark:bg-gray-700"
   >
     <div
-      class="mr-2 basis-[7%] rounded-[8px] border border-surface-950/75"
+      class="mr-3 w-5 shrink-0 rounded-lg border border-surface-950/75"
       :style="{
         backgroundColor: getFieldColor(course.field)
       }"
     ></div>
-    <div class="mr-2 flex basis-[88%]">
-      <div class="flex basis-[16%] flex-col justify-center">
-        <span class="">{{ course.course_code }}</span>
-        <strong class=""
+    <div class="flex flex-1 gap-4 items-center">
+      <div class="w-9 text-sm">
+        <span class="block">{{ course.course_code }}</span>
+        <strong class="block"
           >{{ course.course.credits }} <span class="">Cr</span></strong
         >
       </div>
       <!--Divider-->
-      <div class="m-1 mr-2 min-h-[1em] w-0.5 bg-gray-950"></div>
+      <div class="h-3/4 w-0.5 rounded-lg bg-gray-950"></div>
       <!--Divider-->
-      <div class="basis-[84%] content-center" cols="8">
+      <div class="w-auto" cols="8">
         <CourseDialog
           :field="course.field"
           :course-code="course.course_code"
@@ -30,12 +30,9 @@
     </div>
     <div
       v-if="course.mandatory"
-      class="m-1 max-h-4 basis-[5%] rounded-[6px]"
-      :style="{
-        backgroundColor: 'black'
-      }"
+      class="m-1.5 size-3.5 rounded-md bg-surface-950/75 dark:bg-cics-white"
     ></div>
-  </component>
+  </div>
 </template>
 <script setup lang="ts">
 import CourseDialog from '@/components/dialogs/courses/CourseDialog.vue'
