621d7da0a106a623ecabf4b2ef994fb2ba3d54d5
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-01-17 01:11:27 -0600

    refactor: Update ClassScheduleV1 component with improved table structure and styling

--- app/components/portal/horarios/ClassScheduleV1.vue
+++ app/components/portal/horarios/ClassScheduleV1.vue
@@ -1,40 +1,43 @@
 <template>
-    <table class="elevation-1 rounded-lg" height="650px">
-        <tr class="sticky top-0 z-10 align-top">
-          <th class=""></th>
-            <th v-for="classroom in classrooms" :key="classroom.id" class="alternating border min-w-52">
-                <div class="grid grid-cols-1 text-left pl-2">
-                    <div class="font-light text-sm">
-                        Salón
-                    </div>
-                    <div class="text-3xl font-medium">
-                        {{ classroom.name }}
-                    </div>
-                </div>
-                </col>
-            </th>
-        </tr>
-        <tr v-for="hour in hours" :key="hour.id">
-            <th class="font-medium text-sm pr-8">
-                {{ hour.start_time }} - {{ hour.end_time }}
-            </th>
-            <td class="alternating border border-b border-b-surface-75" v-for="classroom in classrooms" :key="classroom.id">
-                <div v-for="schedule in schedules" :key="schedule.id">
-                    <!--
+  <table class="border-separate border-spacing-0">
+    <thead class=" sticky top-14 z-10">
+      <tr class="">
+        <th class=""></th>
+        <th v-for="classroom in classrooms" :key="classroom.id" class="px-3 py-1 bg-clip-padding bg-white border min-w-32">
+          <div class="text-left  ">
+            <div class="font-normal">
+              Salón
+            </div>
+            <div class="text-lg font-medium">
+              {{ classroom.name }}
+            </div>
+          </div>
+          </col>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="hour in hours" :key="hour.id">
+        <th class="font-medium text-sm p-2 sticky border left-0 z-10 bg-white">
+          {{ hour.start_time }} - {{ hour.end_time }}
+        </th>
+        <td class="alternating border border-b border-b-surface-75" v-for="classroom in classrooms" :key="classroom.id">
+          <div v-for="schedule in schedules" :key="schedule.id">
+            <!--
                     <CursoHorario v-if="
                         schedule.periods[0].hour.start_time == hour.start_time &&
                         schedule.classroom_id == classroom.id
                     " :curso="schedule" />
                     -->
-                    <ScheduleCourseCard v-if="
-                        schedule.periods[0].hour.start_time == hour.start_time &&
-                        schedule.classroom_id == classroom.id
-                    " :title="schedule.career_course.course.name"
-                    />
-                </div>
-            </td>
-        </tr>
-    </table>
+            <ScheduleCourseCard v-if="
+              schedule.periods[0].hour.start_time == hour.start_time &&
+              schedule.classroom_id == classroom.id
+            " :title="schedule.career_course.course.name" />
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup lang="ts">
@@ -43,18 +46,18 @@ import ScheduleCourseCard from '~/components/cards/ScheduleCourseCard.vue';
 import type { Classroom, Course, Hour } from '~/utils/types/schedule-courses'
 
 defineProps<{
-    hours: Array<Hour>
-    classrooms: Array<Classroom>
-    schedules: Array<Course>
+  hours: Array<Hour>
+  classrooms: Array<Classroom>
+  schedules: Array<Course>
 }>()
 </script>
 
 <style scoped lang="postcss">
 .alternating {
-    @apply even:bg-surface-100;
+  @apply even:bg-surface-100;
 }
 
 .border {
-    @apply border-r-[2px] border-r-surface-400;
+  /* @apply border-r-[2px] border-r-surface-400; */
 }
 </style>
