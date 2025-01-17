ed2e857e2a6f7c113eec2a2f89f3ce9c55080c00
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-01-17 15:53:26 -0600

    fix: Create schedule layout, update horarios page to use radix toggle group

--- app/pages/portal/general/horarios.vue
+++ app/pages/portal/general/horarios.vue
@@ -1,38 +1,82 @@
 <template>
-    <div>
-        <div class="grid grid-cols-5">
-            <div class="col-span-1 justify-self-center">
-                <CButton class="h-min" label="Calendario" />
-                <CButton class="h-min" label="Salon" />
+  <div>
+    <div class="grid grid-cols-5">
+      <div class="col-span-1 justify-self-center">
+        <ToggleGroupRoot
+          :model-value="scheduleType"
+          @update:model-value="
+            (val) => {
+              if (val) scheduleType = val as string
+            }
+          "
+          type="single"
+          class="flex gap-1"
+        >
+          <ToggleGroupItem
+            value="calendar"
+            class="rounded-lg transition active:translate-x-0.5 active:translate-y-0.5 border-2 border-transparent px-2.5 py-0.5 data-[state=on]:border-black data-[state=on]:bg-primary-500 data-[state=on]:font-medium data-[state=on]:text-white data-[state=on]:shadow-[1px_1px_0_0_rgba(0,0,0,1)]"
+            >Calendario</ToggleGroupItem
+          >
+          <ToggleGroupItem
+            value="classroom"
+            class="rounded-lg transition active:translate-x-0.5 active:translate-y-0.5 border-2 border-transparent px-2.5 py-0.5 data-[state=on]:border-black data-[state=on]:bg-primary-500 data-[state=on]:font-medium data-[state=on]:text-white data-[state=on]:shadow-[1px_1px_0_0_rgba(0,0,0,1)]"
+            >Salón</ToggleGroupItem
+          >
+        </ToggleGroupRoot>
+
+        <!-- <CButton class="h-min" label="Calendario" />
+        <CButton class="h-min" label="Salon" /> -->
+      </div>
+      <div class="col-span-4">
+        <div
+          class="grid grid-cols-1"
+          v-if="!loadingHours && !loadingClassrooms && !loadingSchedules"
+        >
+          <div class="grid grid-cols-5">
+            <div class="col-span-1">
+              <CButton class="h-min" label="Todas las Carreras" />
             </div>
-            <div class="col-span-4">
-                <div class="grid grid-cols-1" v-if="!loadingHours && !loadingClassrooms && !loadingSchedules">
-                    <div class="grid grid-cols-5">
-                        <div class="col-span-1">
-                            <CButton class="h-min" label="Todas las Carreras"/>
-                        </div>
-                        <div class="col-span-3 justify-self-center">
-                            <ToggleGroupRoot :model-value="scheduleType" @update:model-value="(val) => {
-                                if (val) (scheduleType as any) = val
-                                printThis()
-                            }" type="single" class="flex space-x-2">
-                                <ToggleGroupItem class="" value="clases">
-                                    <CButton class="h-min" label="Clases" :variant="scheduleType == 'clases' ? undefined : 'text'"/>
-                                </ToggleGroupItem>
-                                <ToggleGroupItem class="" value="laboratorios">
-                                    <CButton class="h-min" label="Laboratorios" :variant="scheduleType == 'laboratorios' ? undefined : 'text'"/>
-                                </ToggleGroupItem>
-                            </ToggleGroupRoot>
-                        </div>
-                        <div class="col-span-1">
-                            <CInputText v-model="search" placeholder="Buscar" prepend-icon="lucide:search" no-borders />
-                        </div>
-                    </div>
-                    <ClassScheduleV1 :hours="hours!" :classrooms="classrooms!" :schedules="schedules!" />
-                </div>
+            <div class="col-span-3 justify-self-center">
+              <ToggleGroupRoot
+                :model-value="coursesMode"
+                @update:model-value="
+                  (val) => {
+                    if (val) coursesMode = val as string
+                  }
+                "
+                type="single"
+                class="flex gap-1"
+              >
+                <ToggleGroupItem
+                  value="lectures"
+                  class="rounded-lg transition active:translate-x-0.5 active:translate-y-0.5 border-2 border-transparent px-2.5 py-0.5 data-[state=on]:border-black data-[state=on]:bg-primary-500 data-[state=on]:font-medium data-[state=on]:text-white data-[state=on]:shadow-[1px_1px_0_0_rgba(0,0,0,1)]"
+                  >Clases</ToggleGroupItem
+                >
+                <ToggleGroupItem
+                  value="labs"
+                  class="rounded-lg transition active:translate-x-0.5 active:translate-y-0.5 border-2 border-transparent px-2.5 py-0.5 data-[state=on]:border-black data-[state=on]:bg-primary-500 data-[state=on]:font-medium data-[state=on]:text-white data-[state=on]:shadow-[1px_1px_0_0_rgba(0,0,0,1)]"
+                  >Laboratorios</ToggleGroupItem
+                >
+              </ToggleGroupRoot>
             </div>
+            <div class="col-span-1">
+              <CInputText
+                v-model="search"
+                placeholder="Buscar"
+                prepend-icon="lucide:search"
+                no-borders
+              />
+            </div>
+          </div>
+          <ClassScheduleV1
+            :hours="hours!"
+            :classrooms="classrooms!"
+            :schedules="schedules!"
+          />
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -45,23 +89,24 @@ import type { Classroom, Course, Hour } from '~/utils/types/schedule-courses'
 import { ToggleGroupItem, ToggleGroupRoot } from 'radix-vue'
 
 const { data: hours, pending: loadingHours } =
-    useCustomLazyFetch<Array<Hour>>(`hours`)
+  useCustomLazyFetch<Array<Hour>>(`hours`)
 
 const { data: classrooms, pending: loadingClassrooms } =
-    useCustomLazyFetch<Array<Classroom>>(`classrooms`)
+  useCustomLazyFetch<Array<Classroom>>(`classrooms`)
 
 const { data: schedules, pending: loadingSchedules } =
-    useCustomLazyFetch<Array<Course>>(`schedules/courses`)
-
-const search = ref('');
-const scheduleType = ref('clases')
+  useCustomLazyFetch<Array<Course>>(`schedules/courses`)
 
 definePageMeta({
-    layout: 'default'
+  layout: 'schedule'
 })
+const search = ref('')
+
+const scheduleType = ref('calendar')
+const coursesMode = ref('lectures')
 
 function printThis() {
-    console.log(scheduleType)
+  console.log(scheduleType)
 }
 </script>
 <style scoped lang="scss"></style>
