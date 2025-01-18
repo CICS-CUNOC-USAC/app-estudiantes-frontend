dd07bfd6a33c4e56188d9d43071c7c76ad652b9b
FernandoJRR <fercitotron@gmail.com>
2025-01-18 15:18:59 -0600

    feat: change implementation of schedule to grid

--- app/components/portal/horarios/ClassScheduleV1.vue
+++ app/components/portal/horarios/ClassScheduleV1.vue
@@ -1,4 +1,33 @@
 <template>
+    <div class="grid grid-flow-col auto-cols-max">
+        <div class="grid grid-flow-row auto-rows-max">
+            <div class="min-h-12"></div>
+            <div class="min-h-36 max-w-12 mr-2 text-center" v-for="hour in hours" :key="hour.id">
+                {{ hour.start_time }} - {{ hour.end_time }}
+            </div>
+        </div>
+        <div class="w-52" v-for="classroom in classrooms" :key="classroom.id">
+            <div class="text-left border-r">
+                <div class="font-normal">
+                    Salón
+                </div>
+                <div class="text-lg font-medium">
+                    {{ classroom.name }}
+                </div>
+            </div>
+            <div class="min-h-36 border-b border-r" v-for="hour in hours" :key="hour.id">
+                <template v-for="schedule in schedules" :key="schedule.id">
+                    <ScheduleCourseCard v-if="
+                        schedule.periods[0].hour.start_time == hour.start_time &&
+                        schedule.classroom_id == classroom.id
+                    " :career="schedule.career_course.career.name" :curso="schedule.career_course.course.name"
+                        :seccion="schedule.section.name" :semester="schedule.career_course.semester"
+                        :days="schedule.periods.map(period => period.weekday_id)" />
+                </template>
+            </div>
+        </div>
+    </div>
+    <!--
     <table class="">
         <thead class=" sticky top-14 z-10">
             <tr class="">
@@ -23,7 +52,7 @@
                     {{ hour.start_time }} - {{ hour.end_time }}
                 </th>
                 <td class="alternating border-b border-b-surface-75" v-for="classroom in classrooms"
-                    :key="classroom.id" :rowspan="classroom.id == 2 ? 2 : 1" :class="{'h-32' : classroom.id == 2}">
+                    :key="classroom.id" >
                     <div class="ml-[1px] h-full" v-for="schedule in schedules" :key="schedule.id">
                         <ScheduleCourseCard class="h-full" v-if="
                             schedule.periods[0].hour.start_time == hour.start_time &&
@@ -36,6 +65,7 @@
             </tr>
         </tbody>
     </table>
+    -->
 </template>
 
 <script setup lang="ts">
