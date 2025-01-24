113a4f702c67ce3c244a7250cc2ba36a8ccfc6f4
FernandoJRR <fercitotron@gmail.com>
2025-01-24 10:55:13 -0600

    feat: add schedule implementation with grid plugin

---
+++ app/plugins/grid.client.ts
@@ -0,0 +1,7 @@
+import '@mdi/font/css/materialdesignicons.css'
+// import '../assets/styles/main.scss'
+import VueGridLayout from 'vue-grid-layout-v3'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGridLayout)
+})
