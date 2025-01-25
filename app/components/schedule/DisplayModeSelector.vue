a3cc589bbc9ce2f931aa92e9a00b03fc58f8a97c
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-01-25 15:44:34 -0600

    feat: Add display mode selector for schedule view

---
+++ app/components/schedule/DisplayModeSelector.vue
@@ -0,0 +1,169 @@
+<template>
+  <nav class="flex w-64 flex-col gap-2 self-start justify-self-start">
+    <ToggleGroupRoot
+      :model-value="modelValue"
+      @update:model-value="
+        ($event) => emit('update:modelValue', $event as string)
+      "
+      class="flex gap-1 mb-3"
+    >
+      <ToggleGroupItem
+        value="calendar"
+        class="rounded-lg border-2 border-transparent px-2.5 py-0.5 transition active:translate-x-0.5 active:translate-y-0.5 data-[state=on]:border-black data-[state=on]:bg-primary-500 data-[state=on]:font-medium data-[state=on]:text-white data-[state=on]:shadow-[1px_1px_0_0_rgba(0,0,0,1)]"
+        >Calendario</ToggleGroupItem
+      >
+      <ToggleGroupItem
+        value="classroom"
+        class="rounded-lg border-2 border-transparent px-2.5 py-0.5 transition active:translate-x-0.5 active:translate-y-0.5 data-[state=on]:border-black data-[state=on]:bg-primary-500 data-[state=on]:font-medium data-[state=on]:text-white data-[state=on]:shadow-[1px_1px_0_0_rgba(0,0,0,1)]"
+        >Salón</ToggleGroupItem
+      >
+    </ToggleGroupRoot>
+    <Transition name="slide-view" mode="out-in">
+      <ListboxRoot
+        v-if="modelValue === 'calendar'"
+        class="flex flex-col rounded-lg bg-white"
+        :model-value="testSelectedPeriod"
+        @update:model-value="
+          ($event) => {
+            if ($event) testSelectedPeriod = $event
+          }
+        "
+      >
+        <ListboxContent class="w-full overflow-auto px-2.5 py-4">
+          <Icon name="icon-park-twotone:alarm-clock" class="mx-auto mb-1.5" />
+          <p class="pb-4 text-center text-sm">
+            Selecciona el periodo de tiempo que deseas visualizar en el horario
+          </p>
+          <div class="max-h-96 overflow-auto rounded-xl border p-1.5">
+            <ListboxItem
+              v-for="period in testHours"
+              :key="period.startHour"
+              :value="period"
+              class="group relative flex w-full cursor-pointer select-none items-center rounded-lg py-2.5 leading-none outline-none transition data-[state=checked]:bg-primary-300 data-[disabled]:opacity-50"
+            >
+              <div class="mx-auto">
+                <span class="group-data-[state=checked]:font-semibold">{{
+                  period.startHour
+                }}</span>
+                -
+                <span class="group-data-[state=checked]:font-semibold">{{
+                  period.endHour
+                }}</span>
+              </div>
+            </ListboxItem>
+          </div>
+        </ListboxContent>
+      </ListboxRoot>
+      <div v-else>
+        <ListboxRoot
+          v-if="modelValue === 'classroom'"
+          class="flex flex-col rounded-lg bg-white px-2.5"
+        >
+        <Icon name="icon-park-twotone:pull-door" class="mx-auto mb-1.5" />
+        <p class="pb-4 text-center text-sm">
+          Selecciona el salón de clases que deseas visualizar en el horario
+        </p>
+        <CInputText
+          v-model="classroomSearch"
+          label="Buscar salón"
+          clear-button
+          type="text"
+          prepend-icon="lucide:search"
+          no-borders
+        />
+          <ListboxContent class="w-full py-4">
+            <div class="h-96 overflow-auto rounded-xl border p-1.5">
+              <ListboxItem
+                v-for="classroom in filteredClassrooms"
+                :key="classroom.id"
+                :value="classroom"
+                class="group relative flex w-full cursor-pointer select-none items-center rounded-xl px-3.5 py-2.5 leading-none outline-none transition data-[state=checked]:bg-primary-300 data-[disabled]:opacity-50"
+              >
+                <div class="mx-auto">
+                  <span class="group-data-[state=checked]:font-semibold">{{
+                    classroom.name
+                  }}</span>
+                </div>
+              </ListboxItem>
+            </div>
+          </ListboxContent>
+        </ListboxRoot>
+      </div>
+    </Transition>
+  </nav>
+</template>
+<script setup lang="ts">
+import type { Classroom } from '~/utils/types/schedule-courses'
+import CInputText from '../primitives/form/CInputText.vue'
+
+const { classrooms } = defineProps<{
+  modelValue: 'calendar' | 'classroom'
+  classrooms?: Classroom[] | null
+}>()
+
+const testHours = [
+  {
+    startHour: '14:00',
+    endHour: '15:00'
+  },
+  {
+    startHour: '15:00',
+    endHour: '16:00'
+  },
+  {
+    startHour: '16:00',
+    endHour: '17:00'
+  },
+  {
+    startHour: '17:00',
+    endHour: '18:00'
+  },
+  {
+    startHour: '18:00',
+    endHour: '19:00'
+  },
+  {
+    startHour: '19:00',
+    endHour: '20:00'
+  },
+  {
+    startHour: '20:00',
+    endHour: '21:00'
+  },
+  {
+    startHour: '21:00',
+    endHour: '22:00'
+  },
+  {
+    startHour: '22:00',
+    endHour: '23:00'
+  },
+  {
+    startHour: '23:00',
+    endHour: '00:00'
+  }
+]
+
+const classroomSearch = ref('')
+const filteredClassrooms = computed(() =>
+  classrooms?.filter((classroom) =>
+    classroom.name.toLowerCase().includes(classroomSearch.value.toLowerCase())
+  )
+)
+
+const testSelectedPeriod = ref(testHours[0])
+const emit = defineEmits<{
+  'update:modelValue': [value: string]
+}>()
+</script>
+<style lang="postcss" scoped>
+.slide-view-enter-active,
+.slide-view-leave-active {
+  @apply transition-all duration-200;
+}
+
+.slide-view-enter-from,
+.slide-view-leave-to {
+  @apply translate-x-4 opacity-0;
+}
+</style>
