ed93e090d14e78a985beaeeed3915d7c4e3718ac
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-01-24 18:02:10 -0600

    feat: Introduce InConstruction component and update references in portal pages

--- app/pages/portal/recursos/tesarios/mecanica.vue
+++ app/pages/portal/recursos/tesarios/mecanica.vue
@@ -1,10 +1,6 @@
 <template>
-  <in-construction section="Mecánica - Civil" />
+  <InConstruction section="Mecánica - Civil" />
 </template>
-<script>
-import InConstruction from '@/components/extras/in-construction.vue'
-export default {
-  name: 'TesarioCivil',
-  components: { InConstruction }
-}
+<script setup>
+import InConstruction from '~/components/extras/InConstruction.vue'
 </script>
