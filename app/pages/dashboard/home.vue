58204216596a2e1261d909aa41e4a3f3898d81aa
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-02-20 10:18:28 -0600

    feat: enhance dashboard layout and add dynamic statistics display

--- app/pages/dashboard/home.vue
+++ app/pages/dashboard/home.vue
@@ -1,121 +1,142 @@
 <template>
-    <div>
-        <h1 class="text-2xl mb-8">{{ getSaludo() }}, {{ user?.profile.first_name }}!</h1>
+  <div class="pb-8">
+    <h1 class="mb-8 text-2xl">
+      {{ getSaludo() }},
+      <span class="font-medium">{{ user?.profile.first_name ?? '...' }}</span>
+    </h1>
 
-        <h2 class="mb-4">Accesos Directos</h2>
-        <div class="flex flex-wrap justify-between">
-            <CCardAlt interactive class="max-w-60" to="/dashboard/profile">
-                <template #content>
-                    <div class="space-y-4 grid grid-cols-5">
-                        <div class="col-span-1">
-                            <Icon name="icon-park-twotone:user"></Icon>
-                        </div>
-                        <div class="col-span-4">
-                            <div class="grid grid-rows-2">
-                                <h2>Perfil</h2>
-                                <h3>Ver informacion de tu perfil</h3>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-                <template #footer>
-                    <div class="flex items-center gap-x-2 self-end">
-                        <CButton label="Ver más" icon="icon-park-outline:arrow-right" variant="text"
-                            class="flex-1 text-xs" />
-                        <CButton label="No me interesa" icon="lucide:circle-x" variant="text" class="flex-1 text-xs" />
-                    </div>
-                </template>
-            </CCardAlt>
-            <CCardAlt interactive class="max-w-60" to="/dashboard/pensum/progress">
-                <template #content>
-                    <div class="space-y-4 grid grid-cols-5">
-                        <div class="col-span-1">
-                            <Icon name="icon-park-twotone:chart-line-area"></Icon>
-                        </div>
-                        <div class="col-span-4">
-                            <div class="grid grid-rows-2">
-                                <h2>Progreso Academico</h2>
-                                <h2>Ver tu progreso a lo largo de la carrera</h2>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-                <template #footer>
-                    <div class="flex items-center gap-x-2 self-end">
-                        <CButton label="Ver más" icon="icon-park-outline:arrow-right" variant="text"
-                            class="flex-1 text-xs" />
-                        <CButton label="No me interesa" icon="lucide:circle-x" variant="text" class="flex-1 text-xs" />
-                    </div>
-                </template>
-            </CCardAlt>
-            <CCardAlt interactive class="max-w-60" to="/dashboard/books/physical">
-                <template #content>
-                    <div class="space-y-4 grid grid-cols-5">
-                        <div class="col-span-1">
-                            <Icon name="icon-park-twotone:book"></Icon>
-                        </div>
-                        <div class="col-span-4">
-                            <div class="grid grid-rows-2">
-                                <h2>Biblioteca Fisica</h2>
-                                <h3>Libros disponibles en la biblioteca de Ingenieria</h3>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-                <template #footer>
-                    <div class="flex items-center gap-x-2 self-end">
-                        <CButton label="Ver más" icon="icon-park-outline:arrow-right" variant="text"
-                            class="flex-1 text-xs" />
-                        <CButton label="No me interesa" icon="lucide:circle-x" variant="text" class="flex-1 text-xs" />
-                    </div>
-                </template>
-            </CCardAlt>
-            <CCardAlt interactive class="max-w-60" to="/dashboard/books/digital">
-                <template #content>
-                    <div class="space-y-4 grid grid-cols-5">
-                        <div class="col-span-1">
-                            <Icon name="icon-park-twotone:book-one"></Icon>
-                        </div>
-                        <div class="col-span-4">
-                            <div class="grid grid-rows-2">
-                                <h2>Biblioteca Digital</h2>
-                                <h3>Libros disponibles en la biblioteca digital</h3>
-                            </div>
-                        </div>
-                    </div>
-                </template>
-                <template #footer>
-                    <div class="flex items-center gap-x-2 self-end">
-                        <CButton label="Ver más" icon="icon-park-outline:arrow-right" variant="text"
-                            class="flex-1 text-xs" />
-                        <CButton label="No me interesa" icon="lucide:circle-x" variant="text" class="flex-1 text-xs" />
-                    </div>
-                </template>
-            </CCardAlt>
+    <div class="grid gap-6 md:grid-cols-2">
+      <section>
+        <h2 class="mb-4 font-bold text-muted-color-emphasis">Accesos Directos:</h2>
+        <div class="grid grid-cols-2 gap-4 md:grid-cols-2">
+          <CCardAlt
+            interactive
+            unstyled
+            class="pb-14"
+            v-for="item in items"
+            :key="item.title"
+            :to="item.to"
+          >
+            <template #content>
+              <div class="flex gap-4 flex-col lg:flex-row">
+                <Icon
+                  :name="item.icon"
+                  class="text-primary-600 dark:text-primary-200 shrink-0"
+                  size="26"
+                ></Icon>
+
+                <div class="">
+                  <h2 class="font-bold">{{ item.title }}</h2>
+                  <h3>{{ item.description }}</h3>
+                </div>
+              </div>
+            </template>
+          </CCardAlt>
         </div>
+      </section>
+
+      <section>
+        <h2 class="mb-4 font-bold text-muted-color-emphasis">Estadísticas generales:</h2>
+
+        <section
+          class="grid grid-cols-2 gap-4  overflow-hidden p-5 rounded-xl border border-surface-950/75 bg-surface-50 text-color duration-300 ease-in-out dark:bg-surface-800"
+          v-if="status === 'success' && data"
+        >
+          <div>
+            <h6 class="text-muted-color">Créditos acumulados:</h6>
+            <strong class="text-muted-color-emphasis text-3xl">{{
+              data.current_credits.total_credits
+            }}</strong>
+          </div>
+          <div>
+            <h6 class="text-muted-color">Créditos obligatorios:</h6>
+            <strong class="text-primary-500 text-3xl">{{
+              data.current_credits.mandatory_credits
+            }}</strong>
+            /
+            <span class="text-muted-color-emphasis text-3xl">
+              {{ data.mandatory_credits }}</span
+            >
+          </div>
+          <div>
+            <h6 class="text-muted-color">Créditos optativos:</h6>
+            <strong class="text-primary-500 text-3xl">{{
+              data.current_credits.not_mandatory_credits
+            }}</strong>
+            /
+            <span class="text-muted-color-emphasis text-3xl">
+              {{ data.not_mandatory_credits }}</span
+            >
+          </div>
+          <div>
+            <h6 class="text-muted-color">Créditos disponibles:</h6>
+            <strong class="text-primary-500 text-3xl">{{
+              data.current_credits.total_credits
+            }}</strong>
+            /
+            <span class="text-muted-color-emphasis text-3xl">
+              {{ data.available_credits }}
+            </span>
+          </div>
+        </section>
+      </section>
     </div>
+  </div>
 </template>
 <script setup lang="ts">
 import CCardAlt from '~/components/primitives/card/CCardAlt.vue'
-import { storeToRefs } from 'pinia'
-const authStore = useAuthStore()
-await authStore.fetchUser()
-const regularAuthStore = useRegularAuthStore()
-const { user } = storeToRefs(regularAuthStore)
+
+const { user } = useRegularAuthStore()
+
+const items = [
+  {
+    icon: 'icon-park-twotone:user',
+    title: 'Perfil',
+    description: 'Ver informacion de tu perfil',
+    to: '/dashboard/profile'
+  },
+  {
+    icon: 'icon-park-twotone:chart-line-area',
+    title: 'Progreso Academico',
+    description: 'Ver informacion de tu perfil',
+    to: '/dashboard/pensum/progress'
+  },
+  {
+    icon: 'icon-park-twotone:book',
+    title: 'Biblioteca Fisica',
+    description: 'Libros disponibles en la biblioteca de Ingenieria',
+    to: '/dashboard/books/physical'
+  },
+  {
+    icon: 'icon-park-twotone:book-one',
+    title: 'Biblioteca Digital',
+    description: 'Libros disponibles en la biblioteca digital',
+    to: '/dashboard/books/digital'
+  }
+]
 
 function getSaludo() {
-    const horaActual = new Date().getHours();
-    if (horaActual >= 6 && horaActual < 12) {
-        return "Buenos días"
-    } else if (horaActual >= 12 && horaActual < 19) {
-        return "Buenas tardes"
-    } else {
-        return "Buenas noches"
-    }
+  const horaActual = new Date().getHours()
+  if (horaActual >= 6 && horaActual < 12) {
+    return 'Buenos días'
+  } else if (horaActual >= 12 && horaActual < 19) {
+    return 'Buenas tardes'
+  } else {
+    return 'Buenas noches'
+  }
 }
 
+const { data, status } = await useAsyncData<{
+  current_credits: ProgressCredits
+  mandatory_credits: number
+  not_mandatory_credits: number
+  available_credits: number
+}>(() => $api('/user-courses-progress/stats'), {
+  lazy: true
+})
+
 definePageMeta({
-    layout: 'dashboard'
+  layout: 'dashboard'
 })
 </script>
 <style lang=""></style>
