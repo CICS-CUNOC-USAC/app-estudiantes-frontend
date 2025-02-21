9023e2d4cf1fd9928b02bec58642ea27b1ab7897
Monserrat Gomez <mariagomez202030849@cunoc.edu.gt>
2025-02-21 12:11:17 -0600

    feat: added classroom photos, improved styles with Tailwind, and made layout responsive
    Algunas fotos las voy a cambiar, sin querer las tomé verticalmente.
    Suelen poner algo aquí?

--- app/pages/portal/extras/map.vue
+++ app/pages/portal/extras/map.vue
@@ -9,29 +9,64 @@
             Mapa del Módulo G
         </h1>
 
-        <div class="my-6 space-y-6 h-125 flex items-center justify-center">
-            <div class="flex flex-col m-2">
-                <CButton 
-                    @click="currentLevel++"
-                    icon="icon-park-outline:arrow-up"
-                    class="mb-5 lg:mb-2" 
-                    :disabled="currentLevel == 3"
-                />
-                <CButton 
-                    @click="currentLevel--"
-                    icon="icon-park-outline:arrow-down"
-                    class="mb-5 lg:mb-2" 
-                    :disabled="currentLevel == 1"
-                />
-            </div>
-            <div class="svg-container" 
-            :style="[bgImg, $colorMode.preference === 'dark' ? { filter: 'invert(100%)' } : {}]">
-                <component
-                    class="svg" 
-                    v-bind:style="[$colorMode.preference === 'dark' ? {'filter': 'invert(100%)'} : {}]"
-                    :is="currentSvg" 
+        <div class="flex items-center justify-center">
+            
+            <div v-if="currentRoom" :key="currentRoom" 
+            :class="isSmallScreen
+                ? 'fixed inset-0 flex items-center justify-center bg-black/50 dark:bg-black/75'
+                : 'm-5 lg:mr-20 p-4 bg-neutral-200/60 rounded-sm dark:bg-neutral-900 w-3/4 h-full'
+            ">
+                <div 
+                :class="
+                    isSmallScreen
+                    ? 'bg-white p-4 rounded-lg w-full max-w-md border-3 border-black dark:bg-neutral-900'
+                    : 'rounded-sm dark:bg-neutral-900'
+                ">
+                    <CButton
+                        v-if="isSmallScreen"
+                        class="px-2 mb-5"
+                        icon="icon-park-outline:close-one"
+                        @click="closeModal"
                     />
+                    <div class="flex justify-center items-center">
+                        <img :src="selectedImage" alt="Salon" class="m-2 mx-4 w-auto md:h-80 lg:h-80 h-60">
+                    </div>
+                    
+                    <hr class="my-4 border-t border-neutral-500 dark:border-neutral-700" />
+                    <div>
+                        <p class="text-l font-semibold m-2"> {{ currentRoom }} </p>
+                        <p class="text-s font-light m-2">
+                            <Icon name="icon-park-outline:layers" class="mb-1 mr-1.5 inline-block" />
+                            Nivel {{ currentLevel }}
+                        </p>
+                        <p class="text-s font-light m-2" v-if="isImportant">
+                            <Icon name="icon-park-outline:calendar" class="mb-1 mr-1.5 inline-block" />
+                            Curso actual: CURSO / INGENIERO / HORA
+                        </p>
+                        <p class="text-s font-light m-2" v-if="currentInfo">
+                            <Icon name="icon-park-outline:help" class="mb-1 mr-1.5 inline-block" />
+                            {{ currentInfo }}
+                        </p>
+                    </div>
+                </div>
             </div>
+
+            <div class=" mt-30 md:my-5 lg:my-5 h-102 md:h-115 lg:h-115 flex items-center justify-center">
+                <div class="flex flex-col m-2">
+                    <CButton 
+                    v-show="currentLevel < 3"  @click="currentLevel++;
+                    currentRoom = null" icon="icon-park-outline:arrow-up" class="mb-5 lg:mb-2"
+                    />
+                    <CButton  
+                    v-show="currentLevel > 1"  @click="currentLevel--;
+                    currentRoom = null" icon="icon-park-outline:arrow-down" class="mb-5 lg:mb-2"
+                    />
+                </div>
+                <div class="w-auto h-full  bg-cover bg-no-repeat bg-center" :style="[bgImg]">
+                    <component class="svg w-auto h-full " :is="currentSvg" @click="handleClick" />
+                </div>
+            </div>
+
         </div>
     </main>
 </template>
@@ -47,58 +82,79 @@ import bg_modulo_g1 from '~/assets/img/maps/mg1.png';
 import bg_modulo_g2 from '~/assets/img/maps/mg2.png';
 import bg_modulo_g3 from '~/assets/img/maps/mg3.png';
 
+import { useWindowSize } from '@vueuse/core';
 
+const { width } = useWindowSize();
+const isSmallScreen = computed(() => width.value < 640);
+
+const glob = import.meta.glob('~/assets/img/maps/salones/*.jpg', { eager: true })
+
+const selectedImage = ref()
+const images = Object.fromEntries(
+    Object.entries(glob).map(([key, mod]: any) => {
+        const filename = key.split('/').pop()?.replace('.jpg', '')
+        return [filename, mod.default]
+    })
+)
+
+const selectedPath = ref<SVGElement | null>();
 const currentLevel = ref(1);
- const bgUrl = computed(() => {
-  if (currentLevel.value === 1) return `url(${bg_modulo_g1})`;
-  if (currentLevel.value === 2) return `url(${bg_modulo_g2})`;
-  return `url(${bg_modulo_g3})`;
+const currentRoom = ref();
+const currentInfo = ref();
+const isImportant = ref(false);
+
+const bgUrl = computed(() => {
+    if (currentLevel.value === 1) return `url(${bg_modulo_g1})`;
+    if (currentLevel.value === 2) return `url(${bg_modulo_g2})`;
+    return `url(${bg_modulo_g3})`;
 });
 
 const currentSvg = computed(() => {
-  if (currentLevel.value === 1) return modulo_g1;
-  if (currentLevel.value === 2) return modulo_g2;
-  return modulo_g3;
+    if (currentLevel.value === 1) return modulo_g1;
+    if (currentLevel.value === 2) return modulo_g2;
+    return modulo_g3;
 });
 
 const bgImg = computed(() => ({
-  backgroundImage: bgUrl.value
+    backgroundImage: bgUrl.value
 }));
+
+const handleClick = (event: MouseEvent) => {
+    const target = event.target as SVGElement;
+
+    if (target.tagName === "path") {
+        selectedImage.value = images[target.id];
+
+        if (selectedPath.value) {
+            selectedPath.value.setAttribute("fill", "transparent");
+        }
+
+        target.setAttribute("fill", "var(--p-primary-400)");
+        selectedPath.value = target;
+
+        currentRoom.value = target.querySelector("title")?.textContent || null;
+        currentInfo.value = target.querySelector("desc")?.textContent || null;
+        isImportant.value = target.getAttribute('data-important') === '1';
+    }
+};
+
+const closeModal = () => {
+    if(selectedPath.value){
+        selectedPath.value.setAttribute("fill", "transparent")
+        selectedPath.value = null;
+    }
+    currentRoom.value = null;
+}
 
 </script>
 
 <style>
-.svg-container {
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: auto;
-    height: 100%;
-}
-
-.svg {
-    width: inherit;
-    height: 100%;
-}
-
 .svg path {
     transition: fill 0.1s ease;
 }
 
 .svg path:hover {
-    fill: var(--p-primary-500);
+    fill: var(--p-primary-300);
     cursor: pointer;
-}
-</style>
-<style scoped>
-.fade-slide-enter-active,
-.fade-slide-leave-active {
-    transition: opacity 0.3s ease, transform 0.3s ease;
-}
-
-.fade-slide-enter-from,
-.fade-slide-leave-to {
-    opacity: 0;
-    transform: translateY(10px);
 }
 </style>
