fbc7f3b681f9d41ca1a66c83eea1d042846f89f9
FernandoJRR <fercitotron@gmail.com>
2025-06-19 12:28:48 -0600

    refactor: change physical book creation

--- app/pages/admin/books/create.vue
+++ app/pages/admin/books/create.vue
@@ -7,178 +7,60 @@
       </h1>
     </header>
     <h2 class="my-2 font-medium">Tipo de libro</h2>
-    <PSelectButton
-      v-model="bookType"
-      :options="[
-        { label: 'Digital', value: 'digital' },
-        { label: 'Físico', value: 'physical' }
-      ]"
-      size="small"
-      :allow-empty="false"
-      option-label="label"
-      option-value="value"
-      pt:root:class="grid! grid-cols-2 lg:flex! my-4"
-    >
+    <PSelectButton v-model="bookType" :options="[
+      { label: 'Digital', value: 'digital' },
+      { label: 'Físico', value: 'physical' }
+    ]" size="small" :allow-empty="false" option-label="label" option-value="value"
+      pt:root:class="grid! grid-cols-2 lg:flex! my-4">
       <template #option="{ option }">
         {{ option.label }}
       </template>
     </PSelectButton>
     <section class="mt-4">
       <h2 class="mb-2 font-medium">Información del libro</h2>
-      <PForm
-        :initial-values
-        :resolver="resolverToUse"
-        v-slot="$form"
-        @submit="saveBook"
-        class=""
-      >
+      <PForm :initial-values :resolver="resolverToUse" v-slot="$form" @submit="saveBook" class="">
         <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          <CInputText
-            label="Nombre"
-            name="name"
-            id="name"
-            no-borders
-            prepend-icon="icon-park-twotone:book"
-            :error="$form.name?.error?.message"
-          />
-          <CInputText
-            label="ISBN"
-            name="isbn"
-            id="isbn"
-            no-borders
-            prepend-icon="icon-park-twotone:hashtag-key"
-            :error="$form.isbn?.error?.message"
-          />
-          <CTextarea
-            label="Descripción"
-            name="description"
-            id="description"
-            no-borders
-            :rows="3"
-            prepend-icon="icon-park-twotone:text"
-            :error="$form.description?.error?.message"
-          />
-          <CInputText
-            v-if="bookType === 'physical'"
-            label="Identificador de referencia"
-            name="reference_id"
-            id="reference_id"
-            no-borders
-            prepend-icon="icon-park-twotone:bank-card-two"
-            :error="$form.reference_id?.error?.message"
-          />
-          <CInputText
-            label="Autor"
-            name="author"
-            id="author"
-            no-borders
-            prepend-icon="icon-park-twotone:people"
-            :error="$form.author?.error?.message"
-          />
-          <CInputText
-            v-if="bookType === 'digital'"
-            label="URL de fuente"
-            name="source_url"
-            id="source_url"
-            no-borders
-            prepend-icon="icon-park-outline:link-one"
-            :error="$form.source_url?.error?.message"
-          />
-          <CInputText
-            v-if="bookType === 'physical'"
-            label="Edición"
-            name="edition"
-            id="edition"
-            no-borders
-            prepend-icon="icon-park-twotone:bookshelf"
-            :error="$form.edition?.error?.message"
-          />
+          <CInputText label="Nombre" name="name" id="name" no-borders prepend-icon="icon-park-twotone:book"
+            :error="$form.name?.error?.message" />
+          <CInputText label="ISBN" name="isbn" id="isbn" no-borders prepend-icon="icon-park-twotone:hashtag-key"
+            :error="$form.isbn?.error?.message" />
+          <CTextarea label="Descripción" name="description" id="description" no-borders :rows="3"
+            prepend-icon="icon-park-twotone:text" :error="$form.description?.error?.message" />
+          <CInputText v-if="bookType === 'physical'" label="Identificador de referencia" name="reference_id"
+            id="reference_id" no-borders prepend-icon="icon-park-twotone:bank-card-two"
+            :error="$form.reference_id?.error?.message" />
+          <CInputText label="Autor" name="author" id="author" no-borders prepend-icon="icon-park-twotone:people"
+            :error="$form.author?.error?.message" />
+          <CInputText v-if="bookType === 'digital'" label="URL de fuente" name="source_url" id="source_url" no-borders
+            prepend-icon="icon-park-outline:link-one" :error="$form.source_url?.error?.message" />
+          <CInputText v-if="bookType === 'physical'" label="Edición" name="edition" id="edition" no-borders
+            prepend-icon="icon-park-twotone:bookshelf" :error="$form.edition?.error?.message" />
 
-          <CInputText
-            v-if="bookType === 'physical'"
-            label="Ubicación"
-            name="location"
-            placeholder="Ejemplo: Estante 2, Repisa 5"
-            id="location"
-            no-borders
-            prepend-icon="icon-park-twotone:local"
-            :error="$form.location?.error?.message"
-          />
+          <CInputText v-if="bookType === 'physical'" label="Ubicación" name="location"
+            placeholder="Ejemplo: Estante 2, Repisa 5" id="location" no-borders prepend-icon="icon-park-twotone:local"
+            :error="$form.location?.error?.message" />
 
-          <CSelect
-            label="Categoría"
-            name="category_id"
-            id="category_id"
-            :items="categories?.results"
-            option-label="name"
-            option-value="id"
-            no-borders
-            prepend-icon="icon-park-twotone:tree-list"
-            :error="$form.category_id?.error?.message"
-            />
+          <CSelect label="Categoría" name="category_id" id="category_id" :items="categories?.results || []"
+            option-label="name" option-value="id" no-borders prepend-icon="icon-park-twotone:tree-list"
+            :error="$form.category_id?.error?.message" />
         </div>
 
         <div v-if="bookType === 'physical'" class="mb-4">
           <h5 class="my-3 font-medium">Disponibilidad</h5>
           <section class="grid grid-cols-2 gap-4">
             <div>
-              <PIftaLabel variant="in" class="mb-2">
-                <PInputNumber
-                  inputId="total_availability"
-                  name="total_availability"
-                  variant="filled"
-                  fluid
-                  size="small"
-                  placeholder="0"
-                  input-class="rounded-lg! focus:ring-2! focus:ring-primary-400/50 border-black! dark:border-surface-700! dark:bg-surface-900!"
-                />
-                <label for="total_availability">Disponibilidad total</label>
-              </PIftaLabel>
-              <div
-                v-if="$form.total_availability?.invalid"
-                class="mt-1.5 text-xs font-medium text-red-500"
-              >
-                {{ $form.total_availability?.error?.message }}
-              </div>
-            </div>
-            <div>
-              <PIftaLabel variant="in">
-                <PInputNumber
-                  inputId="current_availability"
-                  name="current_availability"
-                  variant="filled"
-                  fluid
-                  size="small"
-                  placeholder="0"
-                  input-class="rounded-lg! focus:ring-2! focus:ring-primary-400/50 border-black! dark:border-surface-700! dark:bg-surface-900!"
-                />
-                <label for="current_availability">Disponibilidad actual</label>
-              </PIftaLabel>
-              <div
-                v-if="$form.current_availability?.invalid"
-                class="mt-1.5 text-xs font-medium text-red-500"
-              >
-                {{ $form.current_availability?.error?.message }}
-              </div>
+              <PCheckbox inputId="is_available" name="is_available" binary />
+              <span class="ml-2 font-medium">Esta Disponible</span>
             </div>
           </section>
         </div>
 
         <div v-if="bookType === 'digital'" class="mb-4">
           <h5 class="my-2 font-medium">Archivo del libro</h5>
-          <PFileUpload
-            mode="advanced"
-            accept="application/pdf"
-            :multiple="false"
-            pt:root:class="border-none!"
-            @select="(e) => (file = e.files[0])"
-          >
+          <PFileUpload mode="advanced" accept="application/pdf" :multiple="false" pt:root:class="border-none!"
+            @select="(e) => (file = e.files[0])">
             <template #header="{ chooseCallback }">
-              <CButton
-                label="Seleccionar archivo"
-                @click="chooseCallback"
-                variant="tonal"
-              />
+              <CButton label="Seleccionar archivo" @click="chooseCallback" variant="tonal" />
             </template>
             <template #empty>
               <p class="py-2 text-sm">
@@ -196,21 +78,11 @@
           </PFileUpload>
         </div>
         <div class="flex gap-4">
-          <CButton
-            label="Cancelar"
-            icon="icon-park-outline:arrow-left"
-            :to="bookType === 'digital' ? '/admin/books/digital' : '/admin/books/physical'"
-            severity="secondary"
-            class=""
-          />
-          <CButton
-            label="Guardar"
-            icon="icon-park-outline:check"
-            type="submit"
-            class=""
-            :disabled="!file && bookType === 'digital'"
-            :loading
-          />
+          <CButton label="Cancelar" icon="icon-park-outline:arrow-left"
+            :to="bookType === 'digital' ? '/admin/books/digital' : '/admin/books/physical'" severity="secondary"
+            class="" />
+          <CButton label="Guardar" icon="icon-park-outline:check" type="submit" class=""
+            :disabled="!file && bookType === 'digital'" :loading />
         </div>
       </PForm>
     </section>
@@ -238,15 +110,14 @@ const { data: categories } = useAsyncData('categories', () =>
 const file = ref()
 
 const route = useRoute()
-const bookType = ref<'physical' | 'digital'>(route.query.type ?? 'physical')
+const bookType = ref<'physical' | 'digital'>(route.query.type as ('physical' | 'digital') ?? 'physical')
 
 const initialValues = reactive({
   name: '',
   description: '',
   isbn: '',
   reference_id: '',
-  total_availability: null,
-  current_availability: null,
+  is_available: true,
   edition: '',
   location: '',
   author: '',
@@ -279,20 +150,13 @@ const physicalResolver = zodResolver(
     reference_id: z
       .string()
       .nonempty('El identificador de referencia es requerido'),
-    total_availability: z
-      .number({ message: 'El valor debe ser un número' })
-      .int('El valor debe ser un número')
-      .positive('La disponibilidad total debe ser un número positivo'),
-    current_availability: z
-    .number({ message: 'El valor debe ser un número' })
-      .int('El valor debe ser un número')
-      .positive('La disponibilidad actual debe ser un número positivo'),
     edition: z.string().nonempty('La edición del libro es requerida'),
     location: z.string().nonempty('La ubicación del libro es requerida'),
     author: z.string().nonempty('El autor del libro es requerido'),
     category_id: z.number(
       { message: 'La categoría es requerida' }
-    ).int('La categoría es requerida')
+    ).int('La categoría es requerida'),
+    is_available: z.boolean()
   })
 )
 
@@ -303,16 +167,16 @@ const saveBook = async (e: FormSubmitEvent) => {
     const response =
       bookType.value === 'digital'
         ? await createBookItem(
-            {
-              ...(e.values as Omit<BookPayload, 'file'>),
-              file: file.value
-            },
-            bookType.value
-          )
+          {
+            ...(e.values as Omit<BookPayload, 'file'>),
+            file: file.value
+          },
+          bookType.value
+        )
         : await createWithoutMedia(
-            e.values as Omit<BookPayload, 'file'>,
-            bookType.value
-          )
+          e.values as Omit<BookPayload, 'file'>,
+          bookType.value
+        )
 
     if (!response.error) {
       navigateTo(`/admin/books/${bookType.value}`)
