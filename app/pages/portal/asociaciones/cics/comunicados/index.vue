8b5f1f56fa03ecaa5bd93081436cf9ae6ac1693c
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2026-02-18 00:12:17 -0600

    feat: Add CICS communications section and related components

---
+++ app/pages/portal/asociaciones/cics/comunicados/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <main>
+    <nav class="space-x-4">
+      <CButton
+        icon="icon-park-outline:arrow-left"
+        variant="link"
+        label="Regresar al inicio"
+        class="text-muted-color-emphasis mb-4 lg:mb-2"
+        to="/"
+      />
+      <CButton
+        icon="icon-park-outline:paragraph-alphabet"
+        variant="link"
+        label="Regresar a información de la asociación"
+        class="text-muted-color-emphasis mb-4 lg:mb-2"
+        to="/portal/asociaciones/cics"
+      />
+    </nav>
+    <h1 class="py-3 text-xl font-semibold">
+      <Icon name="lucide:megaphone" class="mr-1.5 mb-1 inline-block" />
+      Comunicados del Cómite de Ingeniería en Ciencias y Sistemas
+    </h1>
+    <p class="font-weight-light my-4">
+      En esta sección podrás encontrar todos los comunicados relacionados con el
+      Cómite de Ingeniería en Ciencias y Sistemas. Mantente informado sobre las
+      últimas noticias, eventos y actualizaciones relevantes para los
+      estudiantes de esta carrera.
+    </p>
+    <CInfiniteScroll
+      @load-more="load"
+      v-if="data"
+      :loading="status === 'pending'"
+      :immediate="false"
+    >
+      <div v-if="data" class="grid grid-cols-1 gap-4 lg:grid-cols-3 pb-6">
+        <CCardAlt
+          v-for="post in data.data"
+          :to="`/portal/asociaciones/cics/comunicados/${post.documentId}`"
+          class="hover:bg-primary-200 dark:hover:bg-primary-900/50"
+          interactive-inverse
+          :key="post.documentId"
+        >
+          <template #title>
+            <Icon name="lucide:text" class="mr-1.5 mb-1 inline-block" />
+            <h3 class="py-2 text-lg font-semibold">{{ post.title }}</h3>
+          </template>
+          <template #content>
+            <p class="text-muted-color text-sm">{{ post.description }}</p>
+          </template>
+        </CCardAlt>
+      </div>
+      <p
+        v-if="message"
+        class="text-center text-sm text-muted-color-emphasis mt-4"
+      >
+        {{ message }}
+      </p>
+    </CInfiniteScroll>
+  </main>
+</template>
+<script setup lang="ts">
+import type { Strapi5ResponseMany } from '@nuxtjs/strapi'
+import { toast } from 'vue-sonner'
+import CButton from '~/components/primitives/button/CButton.vue'
+import CCardAlt from '~/components/primitives/card/CCardAlt.vue'
+import CInfiniteScroll from '~/components/primitives/data/CInfiniteScroll.vue'
+import type { Comunicado } from '~/lib/api/strapi/types'
+
+const { data, status } = await useAsyncData<Strapi5ResponseMany<Comunicado>>(
+  'cics-comunicados',
+  () => $strapi('/comunicados', {
+    query: {
+      sort: ['publishedAt:desc'],
+      pagination: {
+        pageSize: 10
+      }
+    }
+  })
+)
+
+const page = ref(1)
+const message = ref('')
+
+async function load() {
+  if (status.value === 'pending') return
+  status.value = 'pending'
+  try {
+    const res = await $strapi<Strapi5ResponseMany<Comunicado>>('/comunicados', {
+      query: {
+        sort: ['publishedAt:desc'],
+        "pagination[page]": page.value + 1,
+        "pagination[pageSize]": 10
+      }
+    })
+    if (res.data && res.data.length > 0 && data.value) {
+      data.value.data.push(...res.data)
+      page.value += 1
+    } else if (res.data && res.data.length === 0) {
+      // No more data to load
+      // toast.warning('No hay más comunicados para mostrar.')
+      message.value = 'No hay más comunicados para mostrar.'
+      setTimeout(() => {
+        message.value = ''
+      }, 3000)
+    }
+  } catch (error) {
+    console.error('Error loading more comunicados:', error)
+    status.value = 'error'
+  } finally {
+    status.value = 'success'
+  }
+}
+</script>
+<style scoped></style>
