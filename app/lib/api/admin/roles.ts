efdbe2de4f66b52510bdee849640de4a6d38dbc9
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2026-04-22 13:04:08 -0600

    feat: implement role editing functionality and update role links

--- app/lib/api/admin/roles.ts
+++ app/lib/api/admin/roles.ts
@@ -7,9 +7,26 @@ export async function getAllRoles(params?: {}) {
   }
 }
 
+export async function fetchRole(id: string | number) {
+  const result = await $api<Role>(`/roles/${id}`)
+  return {
+    result
+  }
+}
+
 export async function createRole (payload: Role) {
   const result = await $api<Role>(`/roles`, {
     method: 'POST',
+    body: payload
+  })
+  return {
+    result
+  }
+}
+
+export async function updateRole(id: string | number, payload: Role) {
+  const result = await $api<Role>(`/roles/${id}`, {
+    method: 'PATCH',
     body: payload
   })
   return {
