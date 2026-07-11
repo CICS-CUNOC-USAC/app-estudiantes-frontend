9894158731bff0c2ee83cc8077e74c2e984c235d
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2026-07-10 22:46:05 -0600

    refactor: remove unused ogImage property from SEO metadata

--- app/components/partials/ElementNotFound.vue
+++ app/components/partials/ElementNotFound.vue
@@ -1,27 +1,15 @@
 <template>
-  <div class="flex items-center gap-2 rounded-md border bg-red-50 px-3 py-2">
-    <Icon name="icon-park-twotone:help" class="text-red-600" />
-    <div class="">
-      <h2 class="font-semibold">{{ title || 'Elemento no encontrado' }}</h2>
-      <p class="text-red-600">
-        {{
-          subtitle ||
-          `Parece que el recurso de ${elementType} que buscas no existe o fue
-        eliminado.`
-        }}
-      </p>
-      <Button
-        class="mt-2"
-        :label="backToLabel"
-        variant="link"
-        icon="icon-park-outline:arrow-left"
-        :to="backToRoute"
-      />
-    </div>
-  </div>
+  <CMessage
+    :title="title || 'Elemento no encontrado'"
+    :subtitle="subtitle || `Parece que el recurso de ${elementType} que buscas no existe o fue eliminado.`"
+    icon="icon-park-twotone:help"
+    :backToRoute="backToRoute"
+    :backToLabel="backToLabel"
+    variant="destructive"
+  />
 </template>
 <script setup lang="ts">
-import Button from '~/components/ui/button/Button.vue'
+import CMessage from './CMessage.vue';
 
 defineProps<{
   elementType?: string
