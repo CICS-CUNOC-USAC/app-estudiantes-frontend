2e54475410b8c08788db9f58cbaa4e86797f255a
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2026-07-12 01:17:09 -0600

    feat: add CurrentAlert component and integrate it into the default layout and update Strapi types with CurrentAlert

--- app/lib/api/strapi/types.ts
+++ app/lib/api/strapi/types.ts
@@ -1,5 +1,10 @@
 import type { BlocksContent } from "vue-strapi-blocks-renderer";
 
+export interface SingleTypeResponse<T> {
+  data: T;
+  meta: Record<string, any>;
+}
+
 export interface BaseContentItem {
   documentId: string;
   createdAt: string;
@@ -26,13 +31,6 @@ export interface Comunicado extends BaseContentItem {
 }
 
 export interface StrapiPostReactionResponse {
-  // counts: {
-  //   like: number;
-  //   love: number;
-  //   haha: number;
-  //   sad: number;
-  //   angry: number;
-  // };
   counts: Record<string, number>;
   userReaction: string | null;
   total: number;
@@ -73,4 +71,22 @@ export const MAPPED_REACTIONS_ICON_COMPONENTS: Record<string, string> = {
   haha: 'fluent-emoji-flat:face-with-tears-of-joy',
   sad: 'fluent-emoji-flat:crying-face',
   angry: 'fluent-emoji-flat:angry-face'
+};
+
+// Alert related
+export type AlertType = 'normal' | 'informacion' | 'advertencia' | 'peligro';
+
+export interface CurrentAlert extends BaseContentItem {
+  title: string;
+  active: boolean;
+  details: BlocksContent;
+  type: AlertType;
+  active_details: boolean;
+}
+
+export const MAPPED_ALERT_TYPES: Record<AlertType, { icon: string; color: string }> = {
+  normal: { icon: 'icon-park-twotone:info', color: 'bg-gray-500/50 dark:bg-gray-600/40' },
+  informacion: { icon: 'icon-park-twotone:info', color: 'bg-cyan-500/50 dark:bg-blue-600/35' },
+  advertencia: { icon: 'icon-park-twotone:attention', color: 'bg-yellow-500/50 dark:bg-yellow-600/40' },
+  peligro: { icon: 'icon-park-twotone:caution', color: 'bg-red-500/50 dark:bg-red-600/40' }
 };
